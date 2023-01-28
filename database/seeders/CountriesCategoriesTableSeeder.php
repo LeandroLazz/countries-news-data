e5a58e17e932416c30e35a8d5b6bf7985c65d584
leandrolazzaretti <leandrolazzaretti@outlook.com>
2023-01-28 18:22:57 +0000

    feature: Add the run method for the CountriesLanguagesSeeder class

--- database/seeders/CountriesCategoriesTableSeeder.php
+++ database/seeders/CountriesCategoriesTableSeeder.php
@@ -5,7 +5,10 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-
+use App\Models\{
+    Country,
+    Category
+};
 
 class CountriesCategoriesTableSeeder extends Seeder
 {
@@ -16,27 +19,21 @@ class CountriesCategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('countries_categories')->insert([
-            // country_id(1) => Belgium, category_id(1) => business, category_id(2) => entertainment
-            ['country_id' => 1, 'category_id' => 1],
-            ['country_id' => 1, 'category_id' => 2],
-
-            // country_id(2) => Canada, category_id(3) => environment, category_id(4) => food, category_id(5) => health
-            ['country_id' => 2, 'category_id' => 3],
-            ['country_id' => 2, 'category_id' => 4],
-            ['country_id' => 2, 'category_id' => 5],
-
-            // country_id(3) => France, category_id(6) => politics, category_id(7) => science
-            ['country_id' => 3, 'category_id' => 6],
-            ['country_id' => 3, 'category_id' => 7],
-
-            // country_id(4) => Germany, category_id(8) => sports, category_id(9) => technology
-            ['country_id' => 4, 'category_id' => 8],
-            ['country_id' => 4, 'category_id' => 9],
-            
-            // country_id(5) => United kingdom, category_id(10) => top, category_id(11) => world
-            ['country_id' => 5, 'category_id' => 10],
-            ['country_id' => 5, 'category_id' => 11],
-        ]);
+       // Create an array of countryCode and categoryName to associate
+       $data = [
+            ['countryCode' => 'be', 'categoryName' => 'business'],
+            ['countryCode' => 'ca', 'categoryName' => 'entertainment'],
+            ['countryCode' => 'ca', 'categoryName' => 'environment'],
+            ['countryCode' => 'fr', 'categoryName' => 'food'],
+            ['countryCode' => 'de', 'categoryName' => 'health'],
+            ['countryCode' => 'gb', 'categoryName' => 'politics']
+        ];
+        
+        // Iterate through the data array and attach a category to a country
+        foreach ($data as $dat) {
+            $country = Country::where('code', $dat['countryCode'])->first();
+            $category = Category::where('name', $dat['categoryName'])->first();
+            $country->categories()->attach($category);
+        }
     }
 }
