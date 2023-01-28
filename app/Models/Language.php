a7c79c578ed3327078bd16078d0b6f15c587182f
leandrolazzaretti <leandrolazzaretti@outlook.com>
2023-01-28 18:20:14 +0000

    feature: Add many-to-many relationship between Language and Country models

--- app/Models/Language.php
+++ app/Models/Language.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Country;
 
 class Language extends Model
 {
     use HasFactory;
+
+    protected $table = 'languages';
+    public $timestamp = false;
+
+    /**
+     * Define a many-to-many relationship with the Country model.
+     * This function returns the countries that are associated with the language through the 'countries_laguages' table.
+     * 
+     */
+    public function countries()
+    {
+        return $this->belongsToMany(Country::class, 'countries_languages');
+    }
 }
