3b20fbc9f9ee7151d0a822bb9d2248748fc05608
leandrolazzaretti <leandrolazzaretti@outlook.com>
2023-01-28 20:26:22 +0000

    feature: implement show method for retrieving country details by code

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -15,11 +15,29 @@ class CountryController extends Controller
     public function index()
     {
        $countries = Country::with(['languages', 'categories'])
-        ->select('id', 'name', 'code')
-        ->get();
+       ->select('id', 'name', 'code')
+       ->get();
        
        return response()->json([
             'countries' => $countries
+        ]);
+    }
+
+    /**
+     * Show the details of a specific country by its code
+     *
+     * @param string $code the country code
+     * 
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($code)
+    {
+        $country = Country::with(['languages', 'categories'])
+        ->select('id', 'name', 'code')
+        ->where('code', $code)->firstOrFail();
+
+        return response()->json([
+            'country' => $country
         ]);
     }
 }
