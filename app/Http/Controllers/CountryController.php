adf680d32774a27796d2021893d1a5f6b7e4a88f
leandrolazzaretti <leandrolazzaretti@outlook.com>
2023-01-28 22:29:17 +0000

    feature: implement addCategory method for adding a new category for a country

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Country;
+use App\Models\{
+    Country,
+    Category
+};
 
 class CountryController extends Controller
 {
@@ -39,5 +42,39 @@ class CountryController extends Controller
         return response()->json([
             'country' => $country
         ]);
+    }
+
+    /**
+     * Add a new category for a specific country 
+     *
+     * @param string $code the country code
+     * @param string $name the category name
+     * 
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function addCategory($code, $categoryName)
+    {
+        // Check if the country exists
+        $country = Country::where('code', $code)->first();
+        if (!$country) {
+            return response()->json(['error' => 'Country not found'], 404);
+        }
+        
+        // Check if the category exists
+        $category = Category::where('name', $categoryName)->first();
+        if (!$category) {
+            return response()->json(['error' => 'Category not found'], 404);
+        }
+
+        // Check if the country already have the category
+        if($country->categories()->where('category_id', $category->id)->exists()){
+            return response()->json(['error' => 'Category already exists in the country'], 400);
+        }        
+
+        $country->categories()->attach($category);
+
+        return response()->json([
+            'message' => 'Category successfully added to country'
+        ], 201);
     }
 }
