8e05ba2887badcb2cfc0829b1e0be25aeff075a4
leandrolazzaretti <leandrolazzaretti@outlook.com>
2023-01-29 17:33:23 +0000

    feature: add comments for functions, and validation of country category

--- app/Http/Controllers/NewsDataController.php
+++ app/Http/Controllers/NewsDataController.php
@@ -15,6 +15,18 @@ class NewsDataController extends Controller
         $this->newsDataService = $newsDataService;
     }
 
+    /**
+     * Retrieve news data from https://newsdata.io/ based on the given country, 
+     * language, and category.
+     * 
+     *
+     * @param string $countryCode
+     * @param string $languageCode
+     * @param string $category
+     * @param int $page
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function index($countryCode, $languageCode, $category, $page = 1)
     {
         $newsData = $this->newsDataService->getNewsData($countryCode, $languageCode, $category, $page);
@@ -24,6 +36,15 @@ class NewsDataController extends Controller
         ], 200);
     }
 
+    /**
+     * Retrieve news data from https://newsdata.io/ based on the given country
+     * 
+     *
+     * @param string $countryCode
+     * @param int $page
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function countryNewsData($countryCode, $page = 1)
     {
         $country = Country::where('code', $countryCode)->first();
@@ -32,6 +53,11 @@ class NewsDataController extends Controller
         if (!$country) {
             return response()->json(['error' => 'Country not found'], 404);
         }
+
+         // Check if the country has a category
+         if(!$country->categories()->exists()){
+            return response()->json(['error' => 'Country does not have any category to search'], 400);
+        }  
 
         $languageCode = $country->languages()->pluck('language')->implode(',');
         $category = $country->categories()->pluck('name')->implode(',');
