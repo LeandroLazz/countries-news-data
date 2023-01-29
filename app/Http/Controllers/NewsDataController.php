1e8ccd49f4a82819038c7572c1dad96f3a7b622d
leandrolazzaretti <leandrolazzaretti@outlook.com>
2023-01-29 17:04:55 +0000

    feature: Add new countryNewsData function

--- app/Http/Controllers/NewsDataController.php
+++ app/Http/Controllers/NewsDataController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Services\NewsDataService;
+use App\Models\Country;
 
 class NewsDataController extends Controller
 {
@@ -16,6 +17,25 @@ class NewsDataController extends Controller
 
     public function index($countryCode, $languageCode, $category, $page = 1)
     {
+        $newsData = $this->newsDataService->getNewsData($countryCode, $languageCode, $category, $page);
+
+        return response()->json([
+            'news' => $newsData
+        ], 200);
+    }
+
+    public function countryNewsData($countryCode, $page = 1)
+    {
+        $country = Country::where('code', $countryCode)->first();
+
+        // Check if the country exists
+        if (!$country) {
+            return response()->json(['error' => 'Country not found'], 404);
+        }
+
+        $languageCode = $country->languages()->pluck('language')->implode(',');
+        $category = $country->categories()->pluck('name')->implode(',');
+
         $newsData = $this->newsDataService->getNewsData($countryCode, $languageCode, $category, $page);
 
         return response()->json([
