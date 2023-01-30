749db8ffbb5477ddeb4ea2c37a0f3d3ad4e9259f
leandrolazzaretti <leandrolazzaretti@outlook.com>
2023-01-30 14:57:54 +0000

    feature: refactor NewsDataService functions to be more readable

--- app/Services/NewsDataService.php
+++ app/Services/NewsDataService.php
@@ -15,98 +15,28 @@ class NewsDataService
         $this->apiKey = env('NEWS_DATA_API_KEY');
     }
 
-     /**
+    /**
      * Retrieve news data from the newsdata.io API.
      *
      * @param string $countryCode 
      * @param string $languageCode
      * @param string $category
      * @param int $page
+     * 
      * @return array
-     * @throws ValidationException
      */
     public function getNewsData($countryCode, $languageCode, $category, $page)
     {
         try {
-            // Validate if that inputs are in the correct format
+            // Validate input data
             $this->validateInput($countryCode, $languageCode, $category, $page);
 
             $newsdataApiObj = new NewsdataApi($this->apiKey);
 
-            // Check if the page is already in cache
-            $cacheKey = "news-data-{$countryCode}-{$languageCode}-{$category}-";
+            // Get news data and next page from cache or API
+            $newsData = $this->getNewsDataFromCacheOrAPI($newsdataApiObj, $countryCode, $languageCode, $category, $page);
 
-             // Pass your desired strings in an array
-            $data = [
-                'country' => $countryCode,
-                'language' => $languageCode,
-                'category' => $category
-            ];
-
-            if ($page == 1) {
-                $newsData = $newsdataApiObj->get_latest_news($data);
-                $nextPage = $newsData->nextPage;
-
-                if ($nextPage) {
-                    $nextPageIndex = $page + 1;
-                    Redis::set($cacheKey.$nextPageIndex, $nextPage);
-                }
-
-                return $newsData;
-            }
-
-            $cacheDataPage = Redis::get($cacheKey.$page);
-
-            if ($cacheDataPage) {
-                $newsData = $newsdataApiObj->get_latest_news(array_merge($data, ['page' => $cacheDataPage]));
-                $nextPage = $newsData->nextPage;
-
-                if ($nextPage) {
-                    $nextPageIndex = $page + 1;
-                    Redis::set($cacheKey.$nextPageIndex, $nextPage);
-                }
-
-                return $newsData;
-            }
-
-            $newsData = $newsdataApiObj->get_latest_news($data);
-            
-            $nextPage = $newsData->nextPage;
-
-            if ($nextPage) {
-                $pageIndex = 2;
-                
-                Redis::set($cacheKey.$pageIndex, $nextPage);
-    
-                while (true) {
-                    $newsData = $newsdataApiObj->get_latest_news(array_merge($data, ['page' => $nextPage]));
-                    
-                    $nextPage = $newsData->nextPage;
-
-                    if ($nextPage) {
-                        $nextPageIndex = $pageIndex+1;
-                        Redis::set($cacheKey.$nextPageIndex, $nextPage);
-                    }
-    
-                    if (!$nextPage || $pageIndex == $page) {
-                        break;
-                    }
-    
-                    $pageIndex++;
-                }
-    
-                if ($pageIndex < $page) {
-                    return response()->json([
-                        'error' => 'This page is not an available page for this request'
-                    ], 400);
-                }
-    
-                return $newsData;
-            } else {
-                return response()->json([
-                    'error' => 'This page is not an available page for this request'
-                ], 400);
-            }
+            return $newsData;
 
         } catch (\Illuminate\Validation\ValidationException $e) {
             return response()->json([
@@ -114,9 +44,117 @@ class NewsDataService
             ], 400);
         } catch (\Exception $e) {
             return response()->json([
-                'error' => 'Failed to retrieve news data from API'
+                'error' => $e->getMessage()
             ], 500);
         }
+    }
+
+    /**
+     * Get news data from cache or API.
+     * 
+     * @param NewsdataApi $newsdataApiObj
+     * @param string $countryCode
+     * @param string $languageCode
+     * @param string $category
+     * @param int $page
+     * 
+     * @return array
+     */
+    private function getNewsDataFromCacheOrAPI($newsdataApiObj, $countryCode, $languageCode, $category, $page)
+    {
+        // Cache key for redis
+        $cacheKey = "news-data-{$countryCode}-{$languageCode}-{$category}-";
+
+        // Request data to be sent to API
+        $requestData = [
+            'country' => $countryCode,
+            'language' => $languageCode,
+            'category' => $category
+        ];
+
+        // Get news data from API if page is 1
+        if ($page == 1) {
+            $newsData = $newsdataApiObj->get_latest_news($requestData);
+            $nextPage = $newsData->nextPage;
+
+            // Store next page in cache if exists
+            if ($nextPage) {
+                $this->storeNextPageInCache($cacheKey, $page + 1, $nextPage);
+            }
+
+            return $newsData;
+        }
+
+        // Get news data from cache
+        $cacheDataPage = Redis::get($cacheKey.$page);
+
+        if ($cacheDataPage) {
+            $newsData = $newsdataApiObj->get_latest_news(array_merge($requestData, ['page' => $cacheDataPage]));
+            $nextPage = $newsData->nextPage;
+
+            // Store next page in cache if exists
+            if ($nextPage) {
+                $this->storeNextPageInCache($cacheKey, $page + 1, $nextPage);
+            }
+
+            return $newsData;
+        }
+
+        // Get news data from API and store all pages in cache
+        $newsData = $this->getNewsDataFromAPIAndStorePagesInCache($newsdataApiObj, $requestData, $cacheKey, $page);
+
+        return $newsData;
+    }
+
+    /**
+     * Get news data from API and store all pages in cache
+     * 
+     * @param NewsdataApi $newsdataApiObj
+     * @param array $requestData
+     * @param string $cacheKey
+     * @param int $page
+     * @param int $currentPageIndex
+     * 
+     * @return array
+     */
+    private function getNewsDataFromAPIAndStorePagesInCache($newsdataApiObj, $requestData, $cacheKey, $page, $currentPageIndex = 1)
+    {
+        $newsData = $newsdataApiObj->get_latest_news($requestData);
+        $nextPage = $newsData->nextPage;
+
+        // Store next page in cache if exists
+        if ($nextPage) {
+            $this->storeNextPageInCache($cacheKey, $currentPageIndex + 1, $nextPage);    
+        }
+
+        if ($nextPage && $currentPageIndex < $page) {
+            $currentPageIndex++;
+            $requestData['page'] = $nextPage;
+            
+            // Recursively call this method until no next page exists
+            $this->getNewsDataFromAPIAndStorePagesInCache($newsdataApiObj, $requestData, $cacheKey, $page, $currentPageIndex);
+        }
+
+        // Validate page
+        if (!$nextPage && $currentPageIndex < $page) {
+            throw new \Exception('This page is not an available page for this request');
+        }
+
+        return $newsData;
+    }
+
+    /**
+     * Store next page in cache.
+     * 
+     * @param string $cacheKey
+     * @param int $pageIndex
+     * @param string $nextPage
+     * 
+     * @return void
+     */
+    private function storeNextPageInCache($cacheKey, $pageIndex, $nextPage)
+    {
+        Redis::set($cacheKey.$pageIndex, $nextPage);
     }
 
     /**
