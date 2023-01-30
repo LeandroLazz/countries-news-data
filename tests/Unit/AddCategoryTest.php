77da5919a4d8eca9d8e209aba5e09ce4bc4e3a27
leandrolazzaretti <leandrolazzaretti@outlook.com>
2023-01-30 17:06:49 +0000

    feature: implement country not found test

--- tests/Unit/AddCategoryTest.php
+++ tests/Unit/AddCategoryTest.php
@@ -3,21 +3,27 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\{
     Country,
     Category
 };
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class AddCategoryTest extends TestCase
 {
+    use RefreshDatabase;
+    
     /**
-     * A basic unit test example.
+     * Test adding a category to a country
      *
      * @return void
      */
-    public function test_example()
+    public function test_country_not_found()
     {
-        $this->assertTrue(true);
+        $response = $this->post('api/countries/XX/categories/food');
+
+        $response->assertJson(['error' => 'Country not found']);
+        $response->assertStatus(404);
     }
+
 }
