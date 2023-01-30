11ceb84bf8d7ff1bdc90d02efe9ea43ce010930b
leandrolazzaretti <leandrolazzaretti@outlook.com>
2023-01-30 17:40:29 +0000

    feature: implement add category to country success test

--- tests/Unit/AddCategoryTest.php
+++ tests/Unit/AddCategoryTest.php
@@ -37,12 +37,24 @@ class AddCategoryTest extends TestCase
     }
 
     /**
+     * Test add category to country success
+     *
+     * @return void
+     */
+    public function test_add_category_to_country_success() {
+        $response = $this->post('api/countries/ca/categories/world');
+
+        $response->assertJson(['message' => 'Category successfully added to country']);
+        $response->assertStatus(201);
+    }
+
+    /**
      * Test category already exists in the country
      *
      * @return void
      */
     public function test_category_already_exists_in_country() {
-        $response = $this->post('api/countries/ca/categories/entertainment');
+        $response = $this->post('api/countries/ca/categories/world');
 
         $response->assertJson(['error' => 'Category already exists in the country']);
         $response->assertStatus(400);
