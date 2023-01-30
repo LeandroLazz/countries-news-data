bae0425e02647c76eee2a4003572ddb69e0d7936
leandrolazzaretti <leandrolazzaretti@outlook.com>
2023-01-30 01:57:24 +0000

    feature: add redis to save pages cache

--- app/Services/NewsDataService.php
+++ app/Services/NewsDataService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use GuzzleHttp\Client;
 use NewsdataIO\NewsdataApi;
+use Illuminate\Support\Facades\Redis;
 
 class NewsDataService
 {
@@ -20,43 +21,93 @@ class NewsDataService
      * @param string $countryCode 
      * @param string $languageCode
      * @param string $category
-     * @param string $page
+     * @param int $page
      * @return array
      * @throws ValidationException
      */
     public function getNewsData($countryCode, $languageCode, $category, $page)
     {
         try {
+            // Validate if that inputs are in the correct format
             $this->validateInput($countryCode, $languageCode, $category, $page);
 
             $newsdataApiObj = new NewsdataApi($this->apiKey);
-            
-            // Pass your desired strings in an array
+
+            // Check if the page is already in cache
+            $cacheKey = "news-data-{$countryCode}-{$languageCode}-{$category}-";
+
+             // Pass your desired strings in an array
             $data = [
                 'country' => $countryCode,
                 'language' => $languageCode,
                 'category' => $category
             ];
-            
+
+            if ($page == 1) {
+                $newsData = $newsdataApiObj->get_latest_news($data);
+                $nextPage = $newsData->nextPage;
+
+                if ($nextPage) {
+                    $nextPageIndex = $page + 1;
+                    Redis::set($cacheKey.$nextPageIndex, $nextPage);
+                }
+
+                return $newsData;
+            }
+
+            $cacheDataPage = Redis::get($cacheKey.$page);
+
+            if ($cacheDataPage) {
+                $newsData = $newsdataApiObj->get_latest_news(array_merge($data, ['page' => $cacheDataPage]));
+                $nextPage = $newsData->nextPage;
+
+                if ($nextPage) {
+                    $nextPageIndex = $page + 1;
+                    Redis::set($cacheKey.$nextPageIndex, $nextPage);
+                }
+
+                return $newsData;
+            }
+
             $newsData = $newsdataApiObj->get_latest_news($data);
-
-            // $pageArray = $newsData->nextPage;
-
-            // while (true) {
-            //     $response = $newsdataApiObj->get_latest_news(array_merge($data, ['page' => $pageArray]));
-            //     // Do something with the response, such as adding it to an array or storing it in a variable
-            //     $pageArray = $response->nextPage;
-
-            //     dd($pageArray);
-            //     if (!$page) {
-            //         break;
-            //     }
-            // }
-
-
-
             
-            return $newsData;
+            $nextPage = $newsData->nextPage;
+
+            if ($nextPage) {
+                $pageIndex = 2;
+                
+                Redis::set($cacheKey.$pageIndex, $nextPage);
+    
+                while (true) {
+                    $newsData = $newsdataApiObj->get_latest_news(array_merge($data, ['page' => $nextPage]));
+                    
+                    $nextPage = $newsData->nextPage;
+
+                    if ($nextPage) {
+                        $nextPageIndex = $pageIndex+1;
+                        Redis::set($cacheKey.$nextPageIndex, $nextPage);
+                    }
+    
+                    if (!$nextPage || $pageIndex == $page) {
+                        break;
+                    }
+    
+                    $pageIndex++;
+                }
+    
+                if ($pageIndex < $page) {
+                    return response()->json([
+                        'error' => 'This page is not an available page for this request'
+                    ], 400);
+                }
+    
+                return $newsData;
+            } else {
+                return response()->json([
+                    'error' => 'This page is not an available page for this request'
+                ], 400);
+            }
+
         } catch (\Illuminate\Validation\ValidationException $e) {
             return response()->json([
                 'error' => $e->validator->messages()
@@ -74,7 +125,7 @@ class NewsDataService
      * @param string $countryCode
      * @param string $languageCode
      * @param string $category
-     * @param string $page
+     * @param int $page
      * @throws ValidationException
      */
     private function validateInput($countryCode, $languageCode, $category, $page)
@@ -88,7 +139,7 @@ class NewsDataService
             'country' => 'required|string|size:2',
             'language' => 'required|string',
             'category' => 'required|string',
-            'page' => 'string',
+            'page' => 'numeric|min:1|max:5',
         ]);
 
         if ($validator->fails()) {
