20d1bf62438cadf371b75cb0dd628d178ebefa26
leandrolazzaretti <leandrolazzaretti@outlook.com>
2023-01-30 17:55:47 +0000

    feature: add remove category from country tests

--- tests/Unit/RemoveCategoryTest.php
+++ tests/Unit/RemoveCategoryTest.php
@@ -2,17 +2,58 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
 
 class RemoveCategoryTest extends TestCase
 {
     /**
-     * A basic unit test example.
+     * Test country not found
      *
      * @return void
      */
-    public function test_example()
+    public function test_country_not_found()
     {
-        $this->assertTrue(true);
+        $response = $this->delete('api/countries/XX/categories/food');
+
+        $response->assertJson(['error' => 'Country not found']);
+        $response->assertStatus(404);
     }
+
+    /**
+     * Test category not found
+     *
+     * @return void
+     */
+    public function test_category_not_found()
+    {
+        $response = $this->delete('api/countries/ca/categories/NonExistentCategory');
+
+        $response->assertJson(['error' => 'Category not found']);
+        $response->assertStatus(404);
+    }
+
+    /**
+     * Test remove category from country success
+     *
+     * @return void
+     */
+    public function test_remove_category_from_country_success() {
+        $response = $this->delete('api/countries/ca/categories/world');
+
+        $response->assertJson(['message' => 'Category successfully removed from country']);
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test country does not have this category
+     *
+     * @return void
+     */
+    public function test_category_does_not_exist_in_country() {
+        $response = $this->delete('api/countries/ca/categories/world');
+
+        $response->assertJson(['error' => 'Country does not have this category']);
+        $response->assertStatus(400);
+    }
+
 }
